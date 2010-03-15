c663a437541bf6facae80780d7f54c228ce8f980
michel.grootjans <michel.grootjans@e963ff9a-3066-11df-8e64-fd3be38ee323>
2010-03-15 22:03:12 +0000

    Removed bin and obj dirs

--- trunk/src/QWars.Win/PlayerView.cs
+++ trunk/src/QWars.Win/PlayerView.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 using QWars.Dummy;
 using QWars.Presentation;
 
@@ -16,47 +17,62 @@ namespace QWars.Win
 
         public object PlayerId { get; set; }
 
-        private void PlayerView_Load(object sender, System.EventArgs e)
+        private void PlayerView_Load(object sender, EventArgs e)
         {
             presenter.Initialize();
         }
 
-        private void btnMug_Click(object sender, System.EventArgs e)
+        private void btnMug_Click(object sender, EventArgs e)
         {
             presenter.MugPedestrian();
         }
 
-        private void btnSellUnusedWeapons_Click(object sender, System.EventArgs e)
+        private void btnSellUnusedWeapons_Click(object sender, EventArgs e)
         {
             presenter.SellUnusedWeapons();
         }
 
-        private void btnBuyWeapon_Click(object sender, System.EventArgs e)
+        private void btnBuyWeapon_Click(object sender, EventArgs e)
         {
             var view = new BuyWeaponsView();
             view.Show();
         }
 
-        private void btnJoinGang_Click(object sender, System.EventArgs e)
+        private void btnJoinGang_Click(object sender, EventArgs e)
         {
             var view = new JoinGangView();
             view.Show();
         }
 
-        private void btnCreateGang_Click(object sender, System.EventArgs e)
+        private void btnCreateGang_Click(object sender, EventArgs e)
         {
             var view = new CreateGangView();
             view.Show();
         }
 
-        private void btnLeaveGang_Click(object sender, System.EventArgs e)
+        private void btnLeaveGang_Click(object sender, EventArgs e)
         {
             presenter.LeaveGang();
         }
 
-        private void btnDisbandGang_Click(object sender, System.EventArgs e)
+        private void btnDisbandGang_Click(object sender, EventArgs e)
         {
             presenter.DisbandGang();
+        }
+
+        public string PlayerName
+        {
+            set { Text = "Player: " + value; }
+        }
+
+        public int Money
+        {
+            set { lblMoney.Text = value.ToString(); }
+        }
+
+        public int XP
+        {
+            set { lblExperience.Text = value.ToString(); }
         }
     }
 }
