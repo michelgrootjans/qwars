ed524d437b18efc1d0591d17ddbe0e46ccde8dd8
michel.grootjans <michel.grootjans@e963ff9a-3066-11df-8e64-fd3be38ee323>
2010-03-15 22:03:32 +0000

    Added dummy implementation

--- trunk/src/QWars.Dummy/PlayerPresenter.cs
+++ trunk/src/QWars.Dummy/PlayerPresenter.cs
@@ -5,21 +5,29 @@ namespace QWars.Dummy
     public class PlayerPresenter : Presenter, IPlayerPresenter
     {
         private readonly IPlayerView view;
+        private int money;
+        private int xp;
 
         public PlayerPresenter(IPlayerView view)
         {
             Log("Constructor");
             this.view = view;
+            money = 0;
+            xp = 0;
         }
 
         public void Initialize()
         {
             Log("Initialize");
+            UpdateView();
         }
 
         public void MugPedestrian()
         {
             Log("MugPedestrian");
+            xp += 100;
+            money += 20;
+            UpdateView();
         }
 
         public void SellUnusedWeapons()
@@ -35,6 +43,13 @@ namespace QWars.Dummy
         public void DisbandGang()
         {
             Log("DisbandGang");
+        }
+
+        private void UpdateView()
+        {
+            view.PlayerName = "Armageddon";
+            view.Money = money;
+            view.XP = xp;
         }
     }
 }
