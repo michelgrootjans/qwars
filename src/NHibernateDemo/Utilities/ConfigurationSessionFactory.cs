65fdde43c9a117bf9e43f87d867c0b70c8ba3327
michel.grootjans <michel.grootjans@e963ff9a-3066-11df-8e64-fd3be38ee323>
2010-05-13 14:25:34 +0000

    Cleanup

--- src/NHibernateDemo/Utilities/ConfigurationSessionFactory.cs
+++ src/NHibernateDemo/Utilities/ConfigurationSessionFactory.cs
@@ -13,9 +13,9 @@ namespace NHibernateDemo.Utilities
         {
             if (sessionFactory == null)
             {
-                /// Initialize NHibernate and builds a session factory
-                /// Note: this is a costly call so it will be executed only once.            
                 NHibernateProfiler.Initialize();
+                // Initialize NHibernate and builds a session factory
+                // Note: this is a costly call so it will be executed only once.            
                 var configuration = new Configuration().Configure();
                 sessionFactory = configuration.BuildSessionFactory();
                 new SchemaExport(configuration).Create(false, true);
